363975460822b58d96358b535d4d48e345b1e110
aferraro776-blip <aferraro776@gmail.com>
2025-12-01 17:33:03 +0100

    Initial commit

---
+++ src/main/java/it/unical/progettoweb/ProgettoWebApplication.java
@@ -0,0 +1,13 @@
+package it.unical.progettoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgettoWebApplication.class, args);
+    }
+
+}
